e5c3886c9029dc393b4c4021a863cfd345fc46e1
Nefo Fortressia <nefothingy@hotmail.com>
2022-06-24 10:14:51 +0700

    Just remove CMAKE_LIBRARY_PATH from make.sh and automatically install the package to the prefix folder of the same name
    Signed-off-by: Nefo Fortressia <nefothingy@hotmail.com>

--- app/jni/cmake.sh
+++ app/jni/cmake.sh
@@ -1,5 +1,5 @@
-#/bin/sh
+#/bin/bash
 
-export CMAKE_LIBRARY_PATH=$CMAKE_LIBRARY_PATH:$(readlink -f ../../libs/expat)
 
-cmake -DCMAKE_TOOLCHAIN_FILE=$(readlink -f ~/Android/Sdk/ndk/23.2.8568313/build/cmake/android.toolchain.cmake) -DANDROID_ABI=x86_64 -DANDROID_PLATFORM=android-29 --install-prefix=$INSTALL_PREFIX
+
+cmake -DCMAKE_TOOLCHAIN_FILE=$(readlink -f ~/Android/Sdk/ndk/23.2.8568313/build/cmake/android.toolchain.cmake) -DANDROID_ABI=$ABI -DANDROID_PLATFORM=android-29 --install-prefix=$(readlink -f "../../libs/$ABI_FULL/${PWD##*/}")
