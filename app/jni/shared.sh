ecd383b3de7ff8552d52df112ff0c9d4e602024b
Nefo Fortressia <nefothingy@hotmail.com>
2022-06-26 14:17:49 +0700

    build: export the project name in shared.sh
    Signed-off-by: Nefo Fortressia <nefothingy@hotmail.com>

--- app/jni/shared.sh
+++ app/jni/shared.sh
@@ -1,8 +1,9 @@
 export PACKAGES_FOLDER=$(readlink -f "../../libs/$ABI_FULL/")
 # Don't mix up .pc files from your host and build target
-export PKGCONFIG_DIRS=$(find $PACKAGES_FOLDER -wholename "**/lib/pkgconfig" -print0 | xargs -0)
+export PKGCONFIG_DIRS=$(find $PACKAGES_FOLDER -wholename "**/pkgconfig" -print0 | xargs -0)
 export PKG_CONFIG_PATH=${PKGCONFIG_DIRS// /:}
-export PREFIX=$(readlink -f "../../libs/$ABI_FULL/${PWD##*/}")
+export PROJECT_NAME=${PWD##*/}
+export PREFIX=$(readlink -f "../../libs/$ABI_FULL/")
 #export INCLUDE_DIRS=$(find $PACKAGES_FOLDER -wholename "**/include" -print0 | xargs -0)
 #export INCLUDE_FLAGS=-I${INCLUDE_DIRS// / -I}
 
