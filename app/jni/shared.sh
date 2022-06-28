9010812f248b98f23fd56a2238f9a333aa14f78a
Nefo Fortressia <nefothingy@hotmail.com>
2022-06-28 08:51:53 +0700

    build: set the default ABI in shared.sh as armv7a-linux-androideabi
    Signed-off-by: Nefo Fortressia <nefothingy@hotmail.com>

--- app/jni/shared.sh
+++ app/jni/shared.sh
@@ -1,3 +1,9 @@
+if [ -z "$ABI_FULL" ]; then
+    export ABI_FULL=armv7a-linux-androideabi
+fi
+
+
+
 export PACKAGES_FOLDER=$(readlink -f "../../libs/$ABI_FULL/")
 # Don't mix up .pc files from your host and build target
 export PKGCONFIG_DIRS=$(find $PACKAGES_FOLDER -wholename "**/pkgconfig" -print0 | xargs -0)
@@ -6,6 +12,6 @@ export PROJECT_NAME=${PWD##*/}
 export PREFIX=$(readlink -f "../../libs/$ABI_FULL/$PROJECT_NAME")
 #export INCLUDE_DIRS=$(find $PACKAGES_FOLDER -wholename "**/include" -print0 | xargs -0)
 #export INCLUDE_FLAGS=-I${INCLUDE_DIRS// / -I}
-
+export PKG_CONFIG_SYSROOT_DIR=""
 #export LIB_DIRS=$(find $PACKAGES_FOLDER -wholename "**/lib" -print0 | xargs -0)
 #export LIB_FLAGS=-L${LIB_DIRS// / -L}
