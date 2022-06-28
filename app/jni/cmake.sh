90373e26ae2bcc4fbe5024669949386f391a908f
Nefo Fortressia <nefothingy@hotmail.com>
2022-06-28 09:42:38 +0700

    build: declare ndk path in shared.sh and make cmake.sh import it
    Signed-off-by: Nefo Fortressia <nefothingy@hotmail.com>

--- app/jni/cmake.sh
+++ app/jni/cmake.sh
@@ -2,4 +2,4 @@
 
 . "$(dirname $0)/shared.sh"
 
-cmake -DCMAKE_TOOLCHAIN_FILE=$(readlink -f ~/Android/Sdk/ndk/23.2.8568313/build/cmake/android.toolchain.cmake) -DANDROID_ABI=$ABI -DANDROID_PLATFORM=android-29 --install-prefix=$PREFIX "$@"
+cmake -DCMAKE_TOOLCHAIN_FILE=$(readlink -f $ANDROID_NDK_PATH/build/cmake/android.toolchain.cmake) -DANDROID_ABI=$ABI -DANDROID_PLATFORM=android-29 --install-prefix=$PREFIX "$@"
--- app/jni/shared.sh
+++ app/jni/shared.sh
@@ -2,6 +2,8 @@ if [ -z "$ABI_FULL" ]; then
     export ABI_FULL=armv7a-linux-androideabi
 fi
 
+export ANDROID_SDK_PATH="~/Android/Sdk"
+export ANDROID_NDK_PATH="$ANDROID_SDK_PATH/ndk/23.2.8568313/"
 
 export PACKAGES_FOLDER=$(readlink -f "$(dirname $0)../../libs/$ABI_FULL/")
 # Don't mix up .pc files from your host and build target
