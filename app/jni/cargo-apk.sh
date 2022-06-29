8fb1579fc5ee60d29ef45ade1a8eebad63c6bc85
Nefo Fortressia <nefothingy@hotmail.com>
2022-06-29 07:23:12 +0700

    build: Pass LD_LINK_ARGS to Rustc
    Signed-off-by: Nefo Fortressia <nefothingy@hotmail.com>

--- app/jni/cargo-apk.sh
+++ app/jni/cargo-apk.sh
@@ -2,8 +2,5 @@
 . "$(dirname $0)/shared.sh"
 
 export LIB_DIRS=$(find $PACKAGES_FOLDER -wholename "**/lib" -print0 | xargs -0)
-export LD_LINK_ARGS=-L${LIB_DIRS// / -L}
-
-echo $LIB_DIRS
-
-cargo apk build
+export LD_LINK_ARGS="-L${LIB_DIRS// / -L} -v"
+RUSTFLAGS="-C link-args=$LD_LINK_ARGS" cargo apk build
