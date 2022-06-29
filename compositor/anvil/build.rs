93757404a19211a2ac56121c137bfafd3c804bea
Nefo Fortressia <nefothingy@hotmail.com>
2022-06-29 07:28:10 +0700

    anvil: disable logind/libseat warning on Android
    Signed-off-by: Nefo Fortressia <nefothingy@hotmail.com>

--- compositor/anvil/build.rs
+++ compositor/anvil/build.rs
@@ -1,12 +1,17 @@
+use cfg_if::cfg_if;
 use std::env::var;
-
 fn main() {
-    if var("CARGO_FEATURE_LOGIND").ok().is_none() && var("CARGO_FEATURE_LIBSEAT").ok().is_none() {
+    cfg_if! {
+    if #[cfg(not(target_os = "android"))] {
+     if var("CARGO_FEATURE_LOGIND").ok().is_none() && var("CARGO_FEATURE_LIBSEAT").ok().is_none() {
         println!("cargo:warning=You are compiling anvil without logind/libseat support.");
         println!("cargo:warning=This means that you'll likely need to run it as root if you want to launch it from a tty.");
         println!("cargo:warning=To enable logind support add `--feature logind` to your cargo invocation.");
         println!("cargo:warning=$ cd anvil; cargo run --feature logind");
         println!("cargo:warning=To enable libseat support add `--feature libseat` to your cargo invocation.");
         println!("cargo:warning=$ cd anvil; cargo run --feature libseat");
+     }
+    }
+
     }
 }
