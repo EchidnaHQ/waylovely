f416d246bee600ed24105f630ad12ad2ee5ace03
Nefo Fortressia <nefothingy@hotmail.com>
2022-06-29 07:15:35 +0700

    build: Switch to aarch64 in the meson crossfile
    Signed-off-by: Nefo Fortressia <nefothingy@hotmail.com>

--- app/jni/meson.sh
+++ app/jni/meson.sh
@@ -3,6 +3,6 @@
 . "$(dirname $0)/shared.sh"
 
 meson _build --pkg-config-path="$PKG_CONFIG_PATH" \
-    --cross-file ../meson-crossfiles/armv7.ini \
+    --cross-file ../meson-crossfiles/aarch64.ini \
     --prefix=$PREFIX \
     -Dc_args="$INCLUDE_FLAGS" -Dcpp_args="$INCLUDE_FLAGS" -Dc_link_args="$LIB_FLAGS" -Dcpp_link_args="$LIB_FLAGS" "$@"
