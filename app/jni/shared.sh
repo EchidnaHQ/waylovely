924139040446f93da2fc2c70736b4176fa204655
Nefo Fortressia <nefothingy@hotmail.com>
2022-06-29 07:26:22 +0700

    build: set the default ABI as arm64-v8a
    Signed-off-by: Nefo Fortressia <nefothingy@hotmail.com>

--- app/jni/shared.sh
+++ app/jni/shared.sh
@@ -1,4 +1,8 @@
 
+if [ -z $ANDROID_ABI ]; then
+    export ANDROID_ABI=arm64-v8a
+fi
+
 if [ $ANDROID_ABI = "arm64-v8a" ]; then
     export ABI_FULL=aarch64-linux-android
     elif [ $ANDROID_ABI = "armv7a-eabi" ]; then
