92df2a4bedd1c5da0ec5ef63fdecf93bd0f018bc
Nefo Fortressia <nefothingy@hotmail.com>
2022-06-30 20:24:41 +0700

    build: Remove the build scripts
    They're moved to https://github.com/EchidnaHQ/simple-and-kawaii now! <3
    
    Signed-off-by: Nefo Fortressia <nefothingy@hotmail.com>

--- deps/configure.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-. "$(dirname $0)/shared.sh"
-. "$(dirname $0)/other.sh"
-echo $PREFIX
-./configure  --host=${ABI_FULL} --target=${ABI_FULL} --with-sysroot=${SYSROOT} --prefix=${PREFIX} "$@"
